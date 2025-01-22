838f356763e7049d90b9610866ab5e0b6f08ca40
Diego M. Maldini Freyre <diegomaldinifreyre@gmail.com>
2025-01-22 08:15:11 -0300

    add first tool

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,7 @@
+import { createResource } from "@/lib/actions/resources";
 import { groq } from "@ai-sdk/groq";
-import { streamText } from "ai";
+import { streamText, tool } from "ai";
+import { z } from "zod";
 
 /* 
 In this code, you declare and export an asynchronous function called POST. You 
@@ -20,6 +22,31 @@ export async function POST(req: Request) {
     any questions. Only respond to questions using information from tool calls. If no 
     relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
     messages,
+
+    /* 
+    In this code, you define a tool called addResource. This tool has three elements:
+    description: description of the tool that will influence when the tool is picked.
+    parameters: Zod schema that defines the parameters necessary for the tool to run.
+    execute: An asynchronous function that is called with the arguments from the tool 
+    call.
+    In simple terms, on each generation, the model will decide whether it should call 
+    the tool. If it deems it should call the tool, it will extract the parameters 
+    from the input and then append a new message to the messages array of type 
+    tool-call. The AI SDK will then run the execute function with the parameters 
+    provided by the tool-call message.
+    */
+    tools: {
+      addResource: tool({
+        description: `add a resource to your knowledge base.
+          If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+        parameters: z.object({
+          content: z
+            .string()
+            .describe("the content or resource to add to the knowledge base"),
+        }),
+        execute: async ({ content }) => createResource({ content }),
+      }),
+    },
   });
 
   return result.toDataStreamResponse();
